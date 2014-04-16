0f4da4f1adfd8d2a3501ca555f770eeed0a79f82
cvanderschuere <cdvconcepts@gmail.com>
2014-04-15 17:26:43 -0700

    Added initial example

--- Http-Demo-iOS/ViewController.m
+++ Http-Demo-iOS/ViewController.m
@@ -18,6 +18,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:@"http://corvallis-bus.appspot.com/routes"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            NSError *jsonError = nil;
+            NSDictionary* responseDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+            if (jsonError != nil) {
+                // Bad JSON -- weird
+            }
+            
+            // Check out response values
+            for (NSDictionary *route in responseDict[@"routes"]) {
+                NSLog(@"Route: %@",route[@"Name"]);
+            }
+        }else{
+            NSLog(@"Error: %@",error);
+        }
+    }] resume];
+  
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
